74ee75952e86d86dc949d05f3ae173a7df5e2e25
RacinesEtRituels <alexandre.boehler@gmail.com>
2025-11-29 18:50:41 +0100

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,13 +1,10 @@
 // ===============================
 // SUPABASE CLIENT
 // ===============================
-import { createClient } from 'https://tjapqrfpgwmsavjtgoml.supabase.co';
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const supabase = createClient(
-  "https://tjapqrfpgwmsavjtgoml.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqYXBxcmZwZ3dtc2F2anRnb21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNzgwMTAsImV4cCI6MjA3ODk1NDAxMH0.lVNkXrzwAaYpm3COxe6zWCQ6y6kz3fjRmZW1FNCy0Lo"
-);
+const supabaseUrl = "https://tjqaprfpgwmsavjtgoml.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqYXBxcmZwZ3dtc2F2anRnb21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNzgwMTAsImV4cCI6MjA3ODk1NDAxMH0.lVNkXrzwAaYpm3COxe6zWCQ6y6kz3fjRmZW1FNCy0Lo"; // remplace par ta public anon key
 
-// Vérifie que tout marche
-console.log("Supabase connecté !");
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
