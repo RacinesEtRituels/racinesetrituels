d661fdddf93f24414889d828faafcc841f875b64
RacinesEtRituels <alexandre.boehler@gmail.com>
2025-11-29 19:57:54 +0100

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://tjqaprfpgwmsavjtgoml.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqYXBxcmZwZ3dtc2F2anRnb21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNzgwMTAsImV4cCI6MjA3ODk1NDAxMH0.lVNkXrzwAaYpm3COxe6zWCQ6y6kz3fjRmZW1FNCy0Lo"
+);
