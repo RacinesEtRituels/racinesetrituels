d24bf139f3c1f141b7bc2a86cc10ee655f7ec49a
RacinesEtRituels <alexandre.boehler@gmail.com>
2025-11-29 18:35:20 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,12 @@
+export function $(selector) {
+  return document.querySelector(selector);
+}
+
+export function showError(msg) {
+  alert("Erreur : " + msg);
+}
+
+export function showSuccess(msg) {
+  alert(msg);
+}
 
