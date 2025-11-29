1e23310779f373ddd20a23b9a5d93ffb0dd1d018
RacinesEtRituels <alexandre.boehler@gmail.com>
2025-11-29 18:27:21 +0100

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,1 +1,13 @@
+// ===============================
+// SUPABASE CLIENT
+// ===============================
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+  "https://YOUR-PROJECT-URL.supabase.co",
+  "YOUR-PUBLIC-ANON-KEY"
+);
+
+// Vérifie que tout marche
+console.log("Supabase connecté !");
 
